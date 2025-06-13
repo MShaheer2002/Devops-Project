957f193e99c8a5ef64000c0cbaee79ce9a52230b
MShaheer2002 <m.shaheershahid12@gmail.com>
2025-06-13 23:43:12 +0500

    Initial commit without tfvars

---
+++ terraform/main.tf
@@ -0,0 +1,78 @@
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-devops"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-devops"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-devops"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "publicip-devops"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                            = "devops-vm"
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = var.location
+  size                            = "Standard_B1s"
+  admin_username                  = var.vm_admin_username
+disable_password_authentication = true
+
+admin_ssh_key {
+  username   = var.vm_admin_username
+  public_key = file(var.ssh_public_key_path)
+}
+  network_interface_ids           = [azurerm_network_interface.nic.id]
+
+  os_disk {
+    name                 = "devops-osdisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "20_04-lts-gen2"
+    version   = "latest"
+  }
+}
+
+output "public_ip" {
+  value = azurerm_public_ip.public_ip.ip_address
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,43 @@
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "client_id" {
+  description = "Azure Client ID (App ID)"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "resource_group" {
+  default = "devops-rg"
+}
+
+variable "vm_admin_username" {
+  default = "azureuser"
+}
+
+variable "vm_admin_password" {
+  description = "Admin password for VM"
+  type        = string
+  sensitive   = true
+}
+
+variable "ssh_public_key_path" {
+  description = "Path to your SSH public key"
+  type        = string
+}
